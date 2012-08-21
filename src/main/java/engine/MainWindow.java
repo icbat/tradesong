bf648639d838e57bac7ccc41789c7193ca2e4e53
Evan Cobb <cobb.evan@gmail.com>
2012-08-20 19:20:46 -0500

    Title BG displays
    Need buttons, then functionality to those buttons

--- src/main/java/engine/MainWindow.java
+++ src/main/java/engine/MainWindow.java
@@ -1,5 +1,7 @@
 package main.java.engine;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
 
 public class MainWindow extends JFrame  {
@@ -8,8 +10,9 @@ public class MainWindow extends JFrame  {
 
 	public MainWindow() {
         setTitle("Hamlet");
+        setBackground(Color.cyan);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(400, 320);
+        setSize(800, 600);
         setLocationRelativeTo(null);
         setVisible(true);
         setResizable(false);
