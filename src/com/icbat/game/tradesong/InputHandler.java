fdac7559b3a3982c6950de882c1c6735ea15799f
Evan Cobb <cobb.evan@gmail.com>
2013-02-15 02:54:51 -0600

    Input set up to handle keypress combos
    Doubt I got this right on the first go, but it's an attempt
    (and it passes the "black-screen-mash-button test")

--- src/com/icbat/game/tradesong/InputHandler.java
+++ src/com/icbat/game/tradesong/InputHandler.java
@@ -1,14 +1,33 @@
 package com.icbat.game.tradesong;
 
+
+import java.util.Set;
+import java.util.TreeSet;
+
 import com.badlogic.gdx.InputAdapter;
 
+/**
+ * Input adapter specialized for this game.
+ * 
+ * For Keys: keeps a log (a set) of what's currently down so processing can occur.
+ * 
+ * TODO needs to be expanded to handle touch screen (when that day comes)
+ * */
 public class InputHandler extends InputAdapter {
+	public Set<Integer> keysDown = new TreeSet<Integer>(); // TreeSet guarantees log(n) add/remove/contains
+	
 	@Override
 	public boolean keyDown(int keycode) {
+		keysDown.add(keycode);
 		Tradesong.log( new Integer(keycode).toString() );
-		
-		
-		return false;
-		
+		return true;
+	}
+	
+	@Override
+	public boolean keyUp(int keycode) {
+		keysDown.remove(keycode);
+		Tradesong.log( new Integer(keycode).toString());
+		Tradesong.log( "Num of Keys down still: " + keysDown.size());
+		return true;
 	}
 }
