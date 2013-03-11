5ce21fecf4d55af781ad718d73e39e9193d30678
Evan Cobb <cobb.evan@gmail.com>
2013-03-11 16:12:50 -0500

    Documented and ready to publish/merge

--- src/com/icbat/game/LJ.java
+++ src/com/icbat/game/LJ.java
@@ -6,77 +6,123 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.Logger;
 
+
+/**
+ * Logging class that extends Gdx.utils.Logger
+ * 
+ * (tries) to log to file automatically from the same commands
+ * 
+ * @author	icbat
+ * */
 public class LJ extends Logger {
 	
 	private FileHandle logfile = null;
 	
 	public LJ() {
 		super("");
-		logfile = Gdx.files.local(makeFileName(""));
+		makeFileWithName("");
 	}
 	
 	public LJ(String tag) {
 		super(tag);
-		logfile = Gdx.files.local(makeFileName(""));
+		makeFileWithName("");
 	}
 	
 	public LJ(String tag, String gameName) {
 		super(tag);
-		logfile = Gdx.files.local(makeFileName(gameName));
+		makeFileWithName(gameName);
 	}
 	
 	@Override
 	public void debug (String message) {
 		super.debug(message);
-		if (logfile != null)
-			logToFile("Debug", message);
+		logToFile("Debug", message);
 	} 
 	
 	@Override
 	public void debug (String message, java.lang.Exception exception) {
 		super.debug(message, exception);
-		if (logfile != null)
-			logToFile("Debug", message, exception);
+		logToFile("Debug", message, exception);
 	} 
 	
 	@Override
 	public void error (String message) {
 		super.error(message);
-		if (logfile != null)
-			logToFile("Error", message);
+		logToFile("Error", message);
 	} 
 	
 	@Override
 	public void error (String message, java.lang.Throwable exception) {
 		super.error(message, exception);
-		if (logfile != null)
-			logToFile("Error", message, exception);
+		logToFile("Error", message, exception);
 	} 
 	
 	@Override
 	public void info (String message) {
 		super.info(message);
-		if (logfile != null)
-			logToFile("Info", message);
+		logToFile("Info", message);
 	} 
 	
 	@Override
 	public void info (String message, java.lang.Exception exception) {
 		super.info(message, exception);
-		if (logfile != null)
-			logToFile("Info", message, exception);
+		logToFile("Info", message, exception);
 	} 
 	
 	// Util
-	private String makeFileName(String gameName) {
-		return "logs/" + gameName + new Date().toString().replaceAll("\\W", ".") + ".log";
+	/**
+	 * TRY's to make a new logfile with relative path/name given as:
+	 * 
+	 * LOCAL:"logs/" + gameName + ".log"
+	 * 
+	 * @param	gameName	Name of the program to help identify where the log was generated
+	 * */
+	private boolean makeFileWithName(String gameName) {
+		//logfile = Gdx.files.local(makeFileName(""));
+		String filename = "logs/" + gameName + new Date().toString().replaceAll("\\W", ".") + ".log";
+		boolean outcome = false;
+		
+		try {
+			logfile = Gdx.files.local(filename);
+			outcome = true;
+		} catch (Exception e) {
+			super.error("Failed to create logging file", e);
+			outcome = false;
+		}
+		
+		return outcome;
 	}
 	
+	/**
+	 * TRY's to write a log message to the open logfile.
+	 * 
+	 * Fails out to the SUPER class (Gdx.utils.Logger)
+	 * 
+	 * @param	tag		Which method called this? Debug, Error, or Info?
+	 * @param	message	The string to be logged
+	 * */
 	private void logToFile(String tag, String message) {
-		this.logfile.writeString(tag + "," + message + ",\n", true);
+		try {
+			this.logfile.writeString(tag + "," + message + ",\n", true);
+		} catch (Exception e) {
+			super.error("Failed to write to file:  " + message, e);
+		}
 	}
 	
+	/**
+	 * TRY's to write a log message to the open log file, concatenating any thrown exception included
+	 * 
+	 * Fails out to the SUPER class's error method
+	 * 
+	 * @param 	tag		Which method called this? Debug, Error, or Info?
+	 * @param	message	The string to be logged
+	 * @param	e		A java Throwable that has been thrown, the reason we're logging an error
+	 * */
 	private void logToFile(String tag, String message, java.lang.Throwable e) {
-		this.logfile.writeString(tag + "," + message + "," + e.toString() + "\n", true);
+		try {
+			this.logfile.writeString(tag + "," + message + "," + e.toString() + "\n", true);
+		} catch (Exception newE) {
+			super.error("Failed to write to file:  " + message + e + newE);
+		}
 	}
 }
