2a65832d3351f0f43b13910a542f2f4b6ca827e3
Evan Cobb <cobb.evan@gmail.com>
2013-03-13 12:38:25 -0500

    Camera was new'ing every render..

--- src/com/icbat/game/tradesong/screens/LevelScreen.java
+++ src/com/icbat/game/tradesong/screens/LevelScreen.java
@@ -58,8 +58,6 @@ public class LevelScreen extends AbstractScreen {
 	
 	@Override
 	public void render(float delta) {
-		camera = new OrthographicCamera();
-		
 		super.render(delta);
 		Gdx.gl.glClearColor(100f / 255f, 100f / 255f, 250f / 255f, 1f);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
