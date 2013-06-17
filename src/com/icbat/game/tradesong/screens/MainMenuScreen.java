df5a837c0bcd4889a92e4c367917b60822adc526
Evan Cobb <cobb.evan@gmail.com>
2013-06-17 14:53:08 -0500

    Two buttons. how to make rows?

--- src/com/icbat/game/tradesong/screens/MainMenuScreen.java
+++ src/com/icbat/game/tradesong/screens/MainMenuScreen.java
@@ -45,10 +45,10 @@ public class MainMenuScreen extends AbstractScreen {
 	
 		// Configure a TextButtonStyle and name it "default". Skin resources are stored by type, so this doesn't overwrite the font.
 		TextButtonStyle textButtonStyle = new TextButtonStyle();
-		textButtonStyle.up = skin.newDrawable("pixel", Color.DARK_GRAY);
-		textButtonStyle.down = skin.newDrawable("pixel", Color.DARK_GRAY);
-		textButtonStyle.checked = skin.newDrawable("pixel", Color.BLUE);
-		textButtonStyle.over = skin.newDrawable("pixel", Color.LIGHT_GRAY);
+//		textButtonStyle.up = skin.newDrawable("pixel", Color.DARK_GRAY);
+//		textButtonStyle.down = skin.newDrawable("pixel", Color.DARK_GRAY);
+//		textButtonStyle.checked = skin.newDrawable("pixel", Color.BLUE);
+//		textButtonStyle.over = skin.newDrawable("pixel", Color.LIGHT_GRAY);
 		textButtonStyle.font = skin.getFont("default");
 		skin.add("default", textButtonStyle);
 	
@@ -58,19 +58,21 @@ public class MainMenuScreen extends AbstractScreen {
 		stage.addActor(table);
 	
 		// Create a button with the "default" TextButtonStyle. A 3rd parameter can be used to specify a name other than "default".
-		final TextButton button = new TextButton("Click me!", skin);
-		table.add(button);
+		final TextButton newButton = new TextButton("Start", skin);
+		final TextButton exitButton = new TextButton("Exit", skin);
+		table.add(newButton);
+		table.add(exitButton);
 	
 		// Add a listener to the button. ChangeListener is fired when the button's checked state changes, eg when clicked,
 		// Button#setChecked() is called, via a key press, etc. If the event.cancel() is called, the checked state will be reverted.
 		// ClickListener could have been used, but would only fire when clicked. Also, canceling a ClickListener event won't
 		// revert the checked state.
-		button.addListener(new ChangeListener() {
-		public void changed (ChangeEvent event, Actor actor) {
-		System.out.println("Clicked! Is checked: " + button.isChecked());
-		button.setText("Good job!");
-		}
-		});
+//		button.addListener(new ChangeListener() {
+//		public void changed (ChangeEvent event, Actor actor) {
+//		System.out.println("Clicked! Is checked: " + button.isChecked());
+//		button.setText("Good job!");
+//		}
+//		});
 	
 		// Add an image actor. Have to set the size, else it would be the size of the drawable (which is the 1x1 texture).
 		table.add(new Image(skin.newDrawable("pixel", Color.RED))).size(64);
