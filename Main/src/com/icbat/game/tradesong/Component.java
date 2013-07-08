8c9b8d84e32bfb9ec412dc09fe24889d2fcce2d2
Evan Cobb <cobb.evan@gmail.com>
2013-07-08 11:22:47 -0500

    Moving away from Entity-Component until it's clear it's needed.

--- Main/src/com/icbat/game/tradesong/Component.java
+++
@@ -1,4 +0,0 @@
-package com.icbat.game.tradesong;
-
-public interface Component {
-}
--- Main/src/com/icbat/game/tradesong/components/RenderableComponent.java
+++
@@ -1,9 +0,0 @@
-package com.icbat.game.tradesong.components;
-
-import com.icbat.game.tradesong.Component;
-
-/**
- * Component that allows items/entities to be drawn to the screen via render
- * */
-public class RenderableComponent implements Component {
-}
