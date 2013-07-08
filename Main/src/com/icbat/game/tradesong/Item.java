f32ddcea432a2d997626fca6d3a76383a4d553c0
Evan Cobb <cobb.evan@gmail.com>
2013-07-08 11:44:20 -0500

    Expanding comment for future reference

--- Main/src/com/icbat/game/tradesong/Item.java
+++ Main/src/com/icbat/game/tradesong/Item.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 
 /**
  * Basic class of all items/objects
+ *
+ * Originally had entity-component design pattern in mind.
+ * decided to go with this instead; consider extracting out later
  * */
 public class Item {
 
     private String itemName;
+
 
     public Item(String itemName) {
         this.itemName = itemName;
