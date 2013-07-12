7022086cd98c5ee3af21b3e35571660bd9c5bb5a
Evan Cobb <cobb.evan@gmail.com>
2013-07-12 12:03:50 -0500

    Scrolling happens with an actor, but is backwards.

--- Main/src/com/icbat/game/tradesong/screens/LevelScreen.java
+++ Main/src/com/icbat/game/tradesong/screens/LevelScreen.java
@@ -1,17 +1,16 @@
 package com.icbat.game.tradesong.screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.renderers.*;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
-import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.icbat.game.tradesong.Item;
 import com.icbat.game.tradesong.Tradesong;
 
@@ -25,8 +24,9 @@ public class LevelScreen extends AbstractScreen {
 	public String mapName = "";
 	private TiledMap map = null;
 	private TiledMapRenderer renderer = null;
-	
 	private OrthographicCamera camera = null;
+
+    private Actor backgroundActor = null;
 
 //    private LinkedList<Item> itemsOnMap = new LinkedList<Item>();
 
@@ -44,17 +44,16 @@ public class LevelScreen extends AbstractScreen {
 		camera.zoom = 1;
 		camera.update();
 
-        // This doesn't seem to be working right now =(
-        this.stage.addListener(new InputListener() {
-            public boolean touchDragged(InputEvent event, float x, float y, int pointer, int button) {
-                log("down");
-                return true;
-            }
 
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                log("up");
-            }
-        });
+        backgroundActor = new Actor();
+        backgroundActor.setTouchable(Touchable.enabled);
+        backgroundActor.setVisible(true);
+        backgroundActor.addListener(new OrthoCamController(camera));
+
+
+        stage.addActor(backgroundActor);
+
+
 		
 		// Map loading Starts
 		game.assets.setLoader(TiledMap.class, new TmxMapLoader(new InternalFileHandleResolver()));
@@ -86,47 +85,59 @@ public class LevelScreen extends AbstractScreen {
 		
 	}
 
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        backgroundActor.setBounds(0,0, width, height);
+    }
+
+
 	@Override
 	public void dispose() {
 		// TODO dispose ALL the things
 		super.dispose(); // Likely needs to be called last
 		this.map.dispose();
 	}
+
+    /**
+     * Input handling for moving camera on maps. Handles:
+     *  - touch-dragging
+     * */
+    class OrthoCamController extends ClickListener {
+        final OrthographicCamera camera;
+        final Vector3 curr = new Vector3();
+        final Vector3 last = new Vector3(-1, -1, -1);
+        final Vector3 delta = new Vector3();
+
+        public OrthoCamController (OrthographicCamera camera) {
+            this.camera = camera;
+        }
+
+        @Override
+        public void touchDragged(InputEvent event, float x, float y, int pointer) {
+            super.touchDragged(event, x, y, pointer);
+            log("Dragging");
+
+            camera.unproject(curr.set(x, y, 0));
+
+            if (!(last.x == -1 && last.y == -1 && last.z == -1)) {
+                camera.unproject(delta.set(last.x, last.y, 0));
+                delta.sub(curr);
+                camera.position.add(delta.x, delta.y, 0);
+            }
+
+            last.set(x, y, 0);
+        }
+
+        @Override
+        public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+            super.touchUp(event, x, y, pointer, button);
+            log("Touch up!");
+            last.set(-1, -1, -1);
+        }
+    }
+
 }
 
-	/**
-	 * Input Adapter for these maps. Directly from GDX test cases for now. Handles:
-	 *  - touch-dragging 
-	 * */
-//	class OrthoCamController extends InputAdapter {
-//		final OrthographicCamera camera;
-//		final Vector3 curr = new Vector3();
-//		final Vector3 last = new Vector3(-1, -1, -1);
-//		final Vector3 delta = new Vector3();
-//
-//		public OrthoCamController (OrthographicCamera camera) {
-//			this.camera = camera;
-//		}
-//
-//		@Override
-//		public boolean touchDragged (int x, int y, int pointer) {
-//			camera.unproject(curr.set(x, y, 0));
-//
-//			if (!(last.x == -1 && last.y == -1 && last.z == -1)) {
-//				camera.unproject(delta.set(last.x, last.y, 0));
-//				delta.sub(curr);
-//				camera.position.add(delta.x, delta.y, 0);
-//			}
-//
-//			last.set(x, y, 0);
-//			return false;
-//		}
-//
-//		@Override
-//		public boolean touchUp (int x, int y, int pointer, int button) {
-//			last.set(-1, -1, -1);
-//			return false;
-//		}
-//	}
-//}
+
 
