74c75b7cb62cc3b1706d6223694858cddabf832d
Evan Cobb <cobb.evan@gmail.com>
2013-07-12 12:40:34 -0500

    More skeleton for items

--- Main/src/com/icbat/game/tradesong/Item.java
+++ Main/src/com/icbat/game/tradesong/Item.java
@@ -1,8 +1,7 @@
 package com.icbat.game.tradesong;
 
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-
-import java.util.ArrayList;
 
 /**
  * Basic class of all items/objects
@@ -13,13 +12,21 @@ import java.util.ArrayList;
 public class Item extends Image {
 
     private String itemName;
+    private String description;
+    private TextureRegion region;
 
 
-    public Item(String itemName) {
+    public Item(String itemName, String description, TextureRegion region) {
         this.itemName = itemName;
+        this.region = region;
+        this.description = description;
 
     }
     public String getItemName() {
         return itemName;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
