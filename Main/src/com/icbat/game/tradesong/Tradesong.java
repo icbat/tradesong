05707c6efc4fee7f0b71d3e3cc8b4b19ddc4c67e
Evan Cobb <cobb.evan@gmail.com>
2013-08-07 03:46:27 -0500

    Beginning refactoring some common-ish asset loading to the initial load

--- Main/src/com/icbat/game/tradesong/Tradesong.java
+++ Main/src/com/icbat/game/tradesong/Tradesong.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
 import com.icbat.game.LJ;
 import com.icbat.game.tradesong.screens.*;
 
@@ -15,9 +16,12 @@ import com.icbat.game.tradesong.screens.*;
  * This class:
  *  - sets up the game initially
  *  - tracks/exposes game variables
+ *  - loads common/global assets
  * */
 public class Tradesong extends Game {
-	
+
+    private static final String PATH_SPRITE_ITEMS = "sprites/items.png";
+    private static final String PATH_SPRITE_FRAME = "sprites/frame.png";
     public GameStateManager gameState;
 	public LJ log = new LJ("", this.getClass().getSimpleName());
     public AssetManager assets = new AssetManager();
@@ -25,17 +29,36 @@ public class Tradesong extends Game {
 	
 	@Override
 	public void create() {
-		log.setLevel( Application.LOG_DEBUG );
-		log.info( "Creating game" );
-		// Some initial logging (type and version)
-		log.info( new Date().toString() );
-		log.info( "App Type" + Gdx.app.getType().toString() );
-		log.info( "Device Version" + Gdx.app.getVersion() );
+        loggingSetup();
+        initializeAssets();
+
         gameState = new GameStateManager(this);
 		goToMainMenu();
 
 
 	}
+
+    private void loggingSetup() {
+        log.setLevel( Application.LOG_DEBUG );
+        log.info( "Creating game" );
+        // Some initial logging (type and version)
+        log.info( new Date().toString() );
+        log.info( "App Type" + Gdx.app.getType().toString() );
+        log.info( "Device Version" + Gdx.app.getVersion() );
+
+    }
+
+    private void initializeAssets() {
+        log.info("Initializing Assets");
+
+        // Item sheet used by all/most icons, items, buttons, etc.
+        this.assets.load(PATH_SPRITE_ITEMS, Texture.class);
+
+        // Frame PNG used in inventory/workshops
+        this.assets.load(PATH_SPRITE_FRAME, Texture.class);
+
+        this.assets.finishLoading(); // Blocks until finished
+    }
 
     public void goBack() {
         if (screenStack.size() > 0) {
@@ -67,5 +90,13 @@ public class Tradesong extends Game {
 
     public void goToWorkshop() {
         goToScreen(new WorkshopScreen(this));
+    }
+
+    public static String getItemsPath() {
+        return PATH_SPRITE_ITEMS;
+    }
+
+    public static String getFramePath() {
+        return PATH_SPRITE_FRAME;
     }
 }
