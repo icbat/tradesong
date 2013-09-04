7f92e0e737c8beb81aebfc164260bda527ca1534
Evan Cobb <cobb.evan@gmail.com>
2013-09-03 20:28:05 -0500

    Moving this back to being in the MainMenuScreen as that class really was doing very little, but it's still nice and clean

--- Main/src/com/icbat/game/tradesong/screens/MainMenuScreen.java
+++ Main/src/com/icbat/game/tradesong/screens/MainMenuScreen.java
@@ -1,8 +1,15 @@
 package com.icbat.game.tradesong.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.icbat.game.tradesong.Tradesong;
-import com.icbat.game.tradesong.stages.MainMenuStage;
+import com.icbat.game.tradesong.stages.AbstractStage;
 
 /**
  * Screen shown first, directs user to other screens/functions
@@ -53,4 +60,77 @@ public class MainMenuScreen extends AbstractScreen {
 //		});
 
 	}
+
+    public static class MainMenuStage extends AbstractStage {
+
+        private final Tradesong gameInstance;
+        private TextButton.TextButtonStyle buttonStyle = new TextButton.TextButtonStyle();
+        private Table table = new Table();
+
+        public MainMenuStage(final Tradesong gameInstance) {
+            this.gameInstance = gameInstance;
+
+            this.buttonStyle.font = new BitmapFont();
+            this.buttonStyle.fontColor = Color.WHITE;
+            this.buttonStyle.overFontColor = Color.BLUE;
+
+            this.table.setFillParent(true);
+            this.addActor(this.table);
+
+            layout();
+        }
+
+        @Override
+        public void layout() {
+
+            table.clear();
+
+            table.add(newStartButton());
+            table.row();
+            table.add(newSettingsButton());
+            table.row();
+            table.add(newExitButton());
+        }
+
+        private TextButton newStartButton() {
+            TextButton startButton = new TextButton("Start game", buttonStyle);
+
+            startButton.addListener(new ChangeListener() {
+                @Override
+                public void changed(ChangeEvent event, Actor actor) {
+                    gameInstance.goToScreen(Tradesong.ScreenTypes.TOWN);
+                }
+            });
+
+            return startButton;
+        }
+
+        private TextButton newSettingsButton() {
+            TextButton settingsButton = new TextButton("Settings", buttonStyle);
+
+            settingsButton.addListener(new ChangeListener() {
+                @Override
+                public void changed(ChangeEvent event, Actor actor) {
+                    gameInstance.goToScreen(Tradesong.ScreenTypes.SETTINGS);
+                }
+            });
+
+            return settingsButton;
+        }
+
+        private TextButton newExitButton() {
+            TextButton exitButton = new TextButton("Exit game", buttonStyle);
+
+            exitButton.addListener(new ChangeListener() {
+                @Override
+                public void changed(ChangeEvent event, Actor actor) {
+                    Gdx.app.exit();
+                }
+            });
+
+            return exitButton;
+        }
+
+
+    }
 }
