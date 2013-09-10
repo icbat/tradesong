04b20352291df5d95c27bbd2368f330a7aeaa198
Evan Cobb <cobb.evan@gmail.com>
2013-09-09 20:24:44 -0500

    Cleaning imports

--- Main/src/com/icbat/game/tradesong/Tradesong.java
+++ Main/src/com/icbat/game/tradesong/Tradesong.java
@@ -1,7 +1,6 @@
 package com.icbat.game.tradesong;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
--- Main/src/com/icbat/game/tradesong/stages/LevelStage.java
+++ Main/src/com/icbat/game/tradesong/stages/LevelStage.java
@@ -1,6 +1,5 @@
 package com.icbat.game.tradesong.stages;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
