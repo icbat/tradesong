a899d414575583d9bf2a070cc4ff5a05b76790e8
icbat <cobb.evan@gmail.com>
2013-12-10 22:44:40 -0600

    Item prototypes load in from XML!

--- Main/src/com/icbat/game/tradesong/Item.java
+++ Main/src/com/icbat/game/tradesong/Item.java
@@ -1,12 +1,12 @@
 package com.icbat.game.tradesong;
 
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 
 /**
  * Item representation with Prototype functionality via copy constructor.
  * */
-public class Item {
+public class Item extends Actor {
     private final String name;
     private final String description;
     private final TextureRegion icon;
