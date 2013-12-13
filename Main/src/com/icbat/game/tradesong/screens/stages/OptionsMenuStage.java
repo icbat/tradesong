28d799fc9500defde3d951f913fdc0fbc3c8c424
icbat <cobb.evan@gmail.com>
2013-12-12 21:55:46 -0600

    Skeleton for table laid out

--- Main/src/com/icbat/game/tradesong/screens/stages/OptionsMenuStage.java
+++ Main/src/com/icbat/game/tradesong/screens/stages/OptionsMenuStage.java
@@ -1,5 +1,44 @@
 package com.icbat.game.tradesong.screens.stages;
 
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
 /***/
 public class OptionsMenuStage extends BaseStage {
+    @Override
+    public void layout() {
+        super.layout();
+
+        Table layout = new Table();
+        layout.add(makeMenuTitleLabel());
+        layout.row();
+        layout.add(makeResumeButton());
+        layout.row();
+        layout.add(makeSaveButton());
+        layout.row();
+        layout.add(makeOptionsButton());
+        layout.row();
+        layout.add(makeExitButton());
+
+    }
+
+    private Actor makeMenuTitleLabel() {
+        return null;
+    }
+
+    private Actor makeResumeButton() {
+        return null;
+    }
+
+    private Actor makeSaveButton() {
+        return null;
+    }
+
+    private Actor makeOptionsButton() {
+        return null;
+    }
+
+    private Actor makeExitButton() {
+        return null;
+    }
 }
