4efa52e33bb5f17ab5cd30d36589cd6d32167f92
icbat <cobb.evan@gmail.com>
2013-12-15 14:34:30 -0600

    Tradesong-52 Crafting Screen Can pick up to drag, but putting down never resets =)

--- Main/src/com/icbat/game/tradesong/screens/stages/InventoryStage.java
+++ Main/src/com/icbat/game/tradesong/screens/stages/InventoryStage.java
@@ -1,6 +1,7 @@
 package com.icbat.game.tradesong.screens.stages;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -8,6 +9,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
 import com.icbat.game.tradesong.Item;
 import com.icbat.game.tradesong.Tradesong;
 import com.icbat.game.tradesong.assetReferences.TextureAssets;
@@ -23,6 +25,7 @@ public class InventoryStage extends BaseStage {
     private Label description;
     private Label itemName;
     Table inventoryTable; // Will likely need to findActor on this from somewhere else.
+    DragAndDrop dragAndDrop = new DragAndDrop();
 
     @Override
     public void layout() {
@@ -74,6 +77,7 @@ public class InventoryStage extends BaseStage {
             frame.setName(String.valueOf(i));
             if (i - 1 < inventoryCopy.size() && inventoryCopy.get(i - 1) != null) {
                 Item item = inventoryCopy.get(i - 1);
+                dragAndDrop.addSource(new ItemSource(item));
                 item.addListener(new InventoryClickListener(item) {
 
                     @Override
@@ -102,9 +106,7 @@ public class InventoryStage extends BaseStage {
 
     protected Image makeFrame(boolean isDropTarget) {
         Image frame =  new Image(Tradesong.getTexture(TextureAssets.FRAME));
-        if (isDropTarget) {
-
-        }
+        dragAndDrop.addTarget(new FrameTarget(frame, isDropTarget));
 
         return frame;
     }
@@ -139,6 +141,63 @@ public class InventoryStage extends BaseStage {
     protected void setItemName(String newName) {
         itemName.setText(newName);
         Gdx.app.debug("itemName dimensions", itemName.getWidth() + ", " + itemName.getHeight());
+    }
+
+    class FrameTarget extends DragAndDrop.Target {
+
+        private boolean validTarget;
+
+        FrameTarget(Actor actor, boolean isValidTarget) {
+            super(actor);
+            validTarget = isValidTarget;
+        }
+
+        @Override
+        /**
+         * What happens when you drag something over this.
+         * */
+        public boolean drag(DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
+            if (validTarget) {
+                getActor().setColor(Color.GREEN);
+            } else {
+                getActor().setColor(Color.RED);
+            }
+            return validTarget;
+        }
+
+        @Override
+        public void reset(DragAndDrop.Source source, DragAndDrop.Payload payload) {
+            super.reset(source, payload);
+            getActor().setColor(Color.BLACK);
+        }
+
+        @Override
+        /**
+         * What happens when you drop something here.
+         * */
+        public void drop(DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
+            // TODO impl
+        }
+    }
+
+    /**
+     * Class for items that can be picked up.
+     * */
+    class ItemSource extends DragAndDrop.Source {
+
+        public ItemSource(Actor actor) {
+            super(actor);
+        }
+
+        @Override
+        public DragAndDrop.Payload dragStart(InputEvent event, float x, float y, int pointer) {
+            DragAndDrop.Payload payload = new DragAndDrop.Payload();
+
+            payload.setObject("some payload"); // This is probably actually the Item, but let's try
+            payload.setDragActor(getActor());
+
+            return payload;  //To change body of implemented methods use File | Settings | File Templates.
+        }
     }
 
 
