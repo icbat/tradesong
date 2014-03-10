7885018e77ccca83a8e8b46795b48f74964949d1
icbat <cobb.evan@gmail.com>
2014-03-09 22:21:21 -0500

    Added sound to popup. Consider moving sound first (and waiting for popup)

--- Main/src/com/icbat/game/tradesong/observation/watchers/EndOfDayWatcher.java
+++ Main/src/com/icbat/game/tradesong/observation/watchers/EndOfDayWatcher.java
@@ -1,18 +1,24 @@
 package com.icbat.game.tradesong.observation.watchers;
 
+import com.badlogic.gdx.audio.Sound;
 import com.icbat.game.tradesong.Tradesong;
 import com.icbat.game.tradesong.actors.EndOfDayPopup;
+import com.icbat.game.tradesong.assetReferences.SoundAssets;
 import com.icbat.game.tradesong.observation.Notification;
 import com.icbat.game.tradesong.observation.Watcher;
 
 /***/
 public class EndOfDayWatcher implements Watcher {
+
+    private final Sound endOfDaySound = Tradesong.getSound(SoundAssets.CHURCHBELL);
+
     @Override
     public void handleNotification(Notification notification) {
         if (shouldAct(notification)) {
             // TODO play sound
             Tradesong.screenManager.getCurrentScreen().addActor(new EndOfDayPopup());
             Tradesong.inventory.addMoney(-200);
+            endOfDaySound.play();
         }
     }
 
