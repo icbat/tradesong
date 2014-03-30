f72002d433c3262c092b68fc43968185e7f95eb7
icbat <cobb.evan@gmail.com>
2014-03-29 21:21:21 -0500

    Basic skeleton exists. This seems _super_ common and smells like it could be refactored somewhere.

--- Main/src/com/icbat/game/tradesong/screens/CraftingScreen.java
+++ Main/src/com/icbat/game/tradesong/screens/CraftingScreen.java
@@ -1,6 +1,22 @@
 package com.icbat.game.tradesong.screens;
 
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
 public class CraftingScreen extends AbstractScreen {
+
+    public CraftingScreen() {
+        Stage craftingStage = new Stage();
+
+        Table layout = new Table();
+        layout.setFillParent(true);
+        craftingStage.addActor(layout);
+
+
+
+        setupStages(craftingStage);
+
+    }
 
     @Override
     public void render(float delta) {
