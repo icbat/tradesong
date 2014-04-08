55e07648315c618ddc2f319dbb5e852d770b6964
icbat <cobb.evan@gmail.com>
2014-04-07 22:57:44 -0500

    Adding backing datastruct to many-to-one (combiner)

---
+++ Main/src/com/icbat/game/tradesong/gameObjects/Workshop.java
@@ -0,0 +1,9 @@
+package com.icbat.game.tradesong.gameObjects;
+
+import java.util.LinkedList;
+
+public class Workshop {
+    LinkedList<CraftingStation> orderedNodes = new LinkedList<CraftingStation>();
+
+
+}
