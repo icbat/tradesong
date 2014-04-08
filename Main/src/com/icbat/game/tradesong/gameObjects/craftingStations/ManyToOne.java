8d25549918265618554c512b2075e8ecccab675a
icbat <cobb.evan@gmail.com>
2014-04-07 22:58:05 -0500

    Adding "descriptive" constructor

--- Main/src/com/icbat/game/tradesong/gameObjects/craftingStations/ManyToOne.java
+++ Main/src/com/icbat/game/tradesong/gameObjects/craftingStations/ManyToOne.java
@@ -2,9 +2,17 @@ package com.icbat.game.tradesong.gameObjects.craftingStations;
 
 import com.icbat.game.tradesong.gameObjects.CraftingStation;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /***/
 public class ManyToOne implements CraftingStation {
     private String name;
+    private HashMap<ArrayList<String>, String> inputToOutput = new HashMap<ArrayList<String>, String>();
+
+    public ManyToOne(String name) {
+        this.name = name;
+    }
 
     @Override
     public String getNodeName() {
