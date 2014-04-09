d4c2f5a698ffb66b30e33b6206df2b1deb5b31d2
icbat <cobb.evan@gmail.com>
2014-04-08 21:06:56 -0500

    Allowing implementations of Stations to change their icons. Also adding icons for stations=.

--- Main/src/com/icbat/game/tradesong/gameObjects/craftingStations/BaseCraftingStation.java
+++ Main/src/com/icbat/game/tradesong/gameObjects/craftingStations/BaseCraftingStation.java
@@ -2,7 +2,7 @@ package com.icbat.game.tradesong.gameObjects.craftingStations;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.NinePatch;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 import com.icbat.game.tradesong.Tradesong;
@@ -15,6 +15,8 @@ public abstract class BaseCraftingStation {
     String name = "";
     LinkedList<String> itemsBeingProcessed = new LinkedList<String>();
     LinkedList<String> readyForOutput = new LinkedList<String>();
+    int iconX = 0;
+    int iconY = 0;
 
     public final String getStationName() {
         return name;
@@ -34,26 +36,21 @@ public abstract class BaseCraftingStation {
     public abstract void process();
 
     public CraftingStationActor getActor() {
-        return new CraftingStationActor(this);
+        return new CraftingStationActor(this, iconX, iconY);
     }
 
     protected static class CraftingStationActor extends Table {
         BaseCraftingStation backingNode;
 
-        public CraftingStationActor(BaseCraftingStation backingNode) {
+        public CraftingStationActor(BaseCraftingStation backingNode, int iconX, int iconY) {
             super(Tradesong.uiStyles);
             this.backingNode = backingNode;
             NinePatchDrawable background = new NinePatchDrawable(new NinePatch(Tradesong.getTexture(TextureAssets.SLIDER_BG), Color.BLACK));
-
             this.setBackground(background);
-            this.add(backingNode.getStationName()).pad(10).space(10);
 
-        }
+            this.add(new Image(TextureAssets.ITEMS.getRegion(iconX,iconY))).pad(10);
+            this.add(backingNode.getStationName()).padRight(10);
 
-        @Override
-        public void draw(SpriteBatch batch, float parentAlpha) {
-            super.draw(batch, parentAlpha);
-            this.debug();
         }
     }
 }
