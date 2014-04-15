09d41aeebda0acb98d0dc604b52ed78fa39a2673
icbat <cobb.evan@gmail.com>
2014-04-14 21:00:33 -0500

    Refactored out a common component I find myself using a lot. Icon that does something on click.

--- Main/src/com/icbat/game/tradesong/screens/components/HudButtonsBlock.java
+++ Main/src/com/icbat/game/tradesong/screens/components/HudButtonsBlock.java
@@ -1,44 +1,27 @@
 package com.icbat.game.tradesong.screens.components;
 
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.icbat.game.tradesong.Tradesong;
-import com.icbat.game.tradesong.assetReferences.TextureAssets;
 import com.icbat.game.tradesong.screens.InventoryScreen;
 import com.icbat.game.tradesong.screens.MidGameControlScreen;
-import com.icbat.game.tradesong.screens.listeners.GoToScreenListener;
 
 public class HudButtonsBlock extends Table {
     public HudButtonsBlock() {
         this.setFillParent(true);
         this.align(Align.left + Align.bottom);
 
-        this.add(new OptionsButton());
-        this.add(new InventoryButton());
-    }
-
-    private class OptionsButton extends Image {
-        private OptionsButton() {
-            super(TextureAssets.ITEMS.getRegion(13,17));
-            this.addListener(new GoToScreenListener() {
-                @Override
-                protected void goToTargetScreen() {
-                    Tradesong.screenManager.goToScreen(new MidGameControlScreen());
-                }
-            });
-        }
-    }
-
-    private class InventoryButton extends Image {
-        private InventoryButton() {
-            super(TextureAssets.ITEMS.getRegion(2,30));
-            this.addListener(new GoToScreenListener() {
-                @Override
-                protected void goToTargetScreen() {
-                    Tradesong.screenManager.goToScreen(new InventoryScreen());
-                }
-            });
-        }
+        this.add(new IconButton(13,17) {
+            @Override
+            void onClick() {
+                Tradesong.screenManager.goToScreen(new MidGameControlScreen());
+            }
+        });
+        this.add(new IconButton(2,30) {
+            @Override
+            void onClick() {
+                Tradesong.screenManager.goToScreen(new InventoryScreen());
+            }
+        });
     }
 }
