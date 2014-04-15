531c16962a1253f73211e62d983503380001b110
icbat <cobb.evan@gmail.com>
2014-04-14 21:29:01 -0500

    Moving HUD to a more logical place.

---
+++ Main/src/com/icbat/game/tradesong/screens/components/HUD.java
@@ -0,0 +1,13 @@
+package com.icbat.game.tradesong.screens.components;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class HUD extends Stage {
+
+    public HUD() {
+        this.addActor(new InfoBlock());
+        this.addActor(new NotificationBlock());
+        this.addActor(new HudButtonsBlock());
+        this.addActor(new ItemDescriptionBlock());
+    }
+}
