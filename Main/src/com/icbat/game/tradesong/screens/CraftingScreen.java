ed8b82637933f12b6dc2500c84a66f2712563274
icbat <cobb.evan@gmail.com>
2014-04-24 19:09:55 -0500

    Disregard drag and drop. Buttons exist for re-arrange now.

--- Main/src/com/icbat/game/tradesong/screens/CraftingScreen.java
+++ Main/src/com/icbat/game/tradesong/screens/CraftingScreen.java
@@ -1,17 +1,16 @@
 package com.icbat.game.tradesong.screens;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
 import com.icbat.game.tradesong.Tradesong;
-import com.icbat.game.tradesong.screens.components.ColorOnHoverListener;
 import com.icbat.game.tradesong.gameObjects.Workshop;
 import com.icbat.game.tradesong.gameObjects.craftingStations.BaseCraftingStation;
+import com.icbat.game.tradesong.screens.components.ColorOnHoverListener;
 
 import java.util.List;
 
@@ -46,8 +45,8 @@ public class CraftingScreen extends BaseInGameScreen {
             super(Tradesong.uiStyles);
             this.setFillParent(true);
             this.add("Worskhop List").colspan(2).row();
-            StationListing currentWorkshop = new StationListing("Current Setup", new Workshop().getOrderedNodes());
-            StationListing possibleStations = new StationListing("Available Stations", Tradesong.craftingStations.getNodesCopy());
+            StationListing currentWorkshop = new StationListing("Current Setup", new Workshop().getOrderedNodes(), true);
+            StationListing possibleStations = new StationListing("Available Stations", Tradesong.craftingStations.getNodesCopy(), false);
             currentWorkshop.linkTo(possibleStations, true);
             possibleStations.linkTo(currentWorkshop, false);
 
@@ -58,57 +57,29 @@ public class CraftingScreen extends BaseInGameScreen {
     }
 
     private class StationListing extends Table {
-        private StationListing(String name, List<BaseCraftingStation> stations) {
+        private StationListing(String name, List<BaseCraftingStation> stations, boolean isRearrangeable) {
             super(Tradesong.uiStyles);
-            this.add(name).row();
+            this.add(name).colspan(3).row();
             for (BaseCraftingStation station : stations) {
                 Actor actor = station.getActor();
+                if (isRearrangeable) {
+                    Label up = new Label("Up", Tradesong.uiStyles);
+                    Label down = new Label("Down", Tradesong.uiStyles);
+
+                    up.addListener(new ColorOnHoverListener(up, Color.PINK));
+                    down.addListener(new ColorOnHoverListener(down, Color.PINK));
+
+                    this.add(up).space(3);
+                    this.add(down).space(3);
+                }
                 this.add(actor).align(Align.left).space(10).prefHeight(64).prefWidth(200).row();
             }
         }
 
         public void linkTo(StationListing otherStations, boolean shouldRemoveWhenAdding) {
-            for(Actor actor : this.getChildren()) {
-                actor.addListener(new ColorOnHoverListener(actor, Color.PINK));
-                dragAndDrop.addSource(new StationSource(actor));
-                dragAndDrop.addTarget(new StationListingTarget(otherStations));
-            }
+//            for(Actor actor : this.getChildren()) {
+//                actor.addListener(new ColorOnHoverListener(actor, Color.PINK));
+//            }
         }
-
-
-        private class StationSource extends DragAndDrop.Source {
-            public StationSource(Actor actor) {
-                super(actor);
-            }
-
-            @Override
-            public DragAndDrop.Payload dragStart(InputEvent event, float x, float y, int pointer) {
-                Gdx.app.debug("you picked me up!", "PUT ME DOWN!");
-                return null;
-            }
-        }
-
-        private class StationListingTarget extends DragAndDrop.Target {
-            public StationListingTarget(StationListing otherStations) {
-                super(otherStations);
-            }
-
-            @Override
-            public boolean drag(DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
-                return false;
-            }
-
-            @Override
-            public void drop(DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
-
-            }
-
-            @Override
-            public void reset(DragAndDrop.Source source, DragAndDrop.Payload payload) {
-                super.reset(source, payload);
-                source.getActor().setColor(Color.WHITE);
-            }
-        }
-
     }
 }
