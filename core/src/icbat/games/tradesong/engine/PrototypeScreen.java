2ac94400d5bf828af83b59538239e44a062f857e
icbat <cobb.evan@gmail.com>
2015-03-08 15:34:12 -0500

    Making new screen to do super-early playtesting

---
+++ core/src/icbat/games/tradesong/engine/PrototypeScreen.java
@@ -0,0 +1,44 @@
+package icbat.games.tradesong.engine;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+
+/***/
+public class PrototypeScreen implements Screen {
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
