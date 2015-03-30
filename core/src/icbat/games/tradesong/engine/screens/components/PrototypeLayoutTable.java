2208be29bdb4e5961a6389c9b8ef3dfc26492ead
icbat <cobb.evan@gmail.com>
2015-03-29 21:03:55 -0500

    extracting out listeners for add/remove workshops

--- core/src/icbat/games/tradesong/engine/screens/components/PrototypeLayoutTable.java
+++ core/src/icbat/games/tradesong/engine/screens/components/PrototypeLayoutTable.java
@@ -1,11 +1,8 @@
 package icbat.games.tradesong.engine.screens.components;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.Align;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import icbat.games.tradesong.TradesongGame;
 import icbat.games.tradesong.game.PlayerHoldings;
 import icbat.games.tradesong.game.TurnTaker;
@@ -62,35 +59,6 @@ public class PrototypeLayoutTable extends Table {
             activeDisplay.row();
         }
         return activeDisplay;
-    }
-
-    private class RemoveWorkshopListener extends ClickListener {
-        private final Workshop workshop;
-
-        public RemoveWorkshopListener(Workshop workshop) {
-            this.workshop = workshop;
-        }
-
-        @Override
-        public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-            Gdx.app.log("add button", "clicked!");
-            holdings.removeWorkshop(workshop);
-            return super.touchDown(event, x, y, pointer, button);
-        }
-    }
-
-    private class AddWorkshopListener extends ClickListener {
-        private final Workshop workshop;
-
-        public AddWorkshopListener(Workshop workshop) {
-            this.workshop = workshop;
-        }
-
-        @Override
-        public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-            holdings.addWorkshop(workshop.spawnClone());
-            return super.touchDown(event, x, y, pointer, button);
-        }
     }
 
 
