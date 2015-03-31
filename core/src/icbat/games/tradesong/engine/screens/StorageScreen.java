5a440574d3501bedf37a1dceff185f4a5a4bd788
icbat <cobb.evan@gmail.com>
2015-03-31 00:17:49 -0500

    adding display of current workers

--- core/src/icbat/games/tradesong/engine/screens/StorageScreen.java
+++ core/src/icbat/games/tradesong/engine/screens/StorageScreen.java
@@ -18,8 +18,12 @@ public class StorageScreen extends AbstractBaseScreen {
         Table layout = new Table(TradesongGame.skin);
         layout.add("Items in storage:").space(15);
         layout.add("" + TradesongGame.holdings.getStorage().size()).space(15).row();
-        layout.add(new AddWorkersButton(TradesongGame.holdings.getStorage().getWorkers(), TradesongGame.holdings.getSpareWorkers()));
-        layout.add(new RemoveWorkersButton(TradesongGame.holdings.getStorage().getWorkers(), TradesongGame.holdings.getSpareWorkers())).row();
+
+        layout.add("Workers assigned:").space(15);
+        layout.add("" + TradesongGame.holdings.getStorage().getWorkers().size()).space(15).row();
+
+        layout.add(new AddWorkersButton(TradesongGame.holdings.getStorage().getWorkers(), TradesongGame.holdings.getSpareWorkers())).space(15);
+        layout.add(new RemoveWorkersButton(TradesongGame.holdings.getStorage().getWorkers(), TradesongGame.holdings.getSpareWorkers())).space(15).row();
 
         final StorageDisplay storageDisplay = new StorageDisplay();
         layout.add(storageDisplay).colspan(2).space(15);
