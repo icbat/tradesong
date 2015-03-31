a4f9eaa7a5765f350f756218a4bfb577e8d35844
icbat <cobb.evan@gmail.com>
2015-03-30 22:26:59 -0500

    Reverting the last change for those I mistook Workers for Workshop

--- core/src/icbat/games/tradesong/engine/screens/WorkshopScreen.java
+++ core/src/icbat/games/tradesong/engine/screens/WorkshopScreen.java
@@ -3,7 +3,7 @@ package icbat.games.tradesong.engine.screens;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import icbat.games.tradesong.TradesongGame;
-import icbat.games.tradesong.engine.screens.components.BuyWorkersButton;
+import icbat.games.tradesong.engine.screens.components.AddWorkersButton;
 import icbat.games.tradesong.engine.screens.components.RemoveWorkersButton;
 import icbat.games.tradesong.game.workshops.Workshop;
 
@@ -20,7 +20,7 @@ public class WorkshopScreen extends AbstractBaseScreen {
     protected Table buildCentralLayout() {
         final Table layout = new Table(TradesongGame.skin);
         layout.add(new Label("Assigned Workers:" + workshop.getWorkers().size(), TradesongGame.skin)).colspan(2).row();
-        layout.add(new BuyWorkersButton(workshop, TradesongGame.holdings.getSpareWorkers())).pad(15);
+        layout.add(new AddWorkersButton(workshop, TradesongGame.holdings.getSpareWorkers())).pad(15);
         layout.add(new RemoveWorkersButton(workshop, TradesongGame.holdings.getSpareWorkers())).pad(15);
         return layout;
     }
