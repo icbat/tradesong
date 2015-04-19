0a5e6f2ab1de6688e86c70e3cdd7ffac584bf0ed
icbat <cobb.evan@gmail.com>
2015-04-19 16:21:51 -0500

    Items are read in through json =)

--- core/src/icbat/games/tradesong/TradesongGame.java
+++ core/src/icbat/games/tradesong/TradesongGame.java
@@ -3,10 +3,8 @@ package icbat.games.tradesong;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import icbat.games.tradesong.engine.GameSkin;
-import icbat.games.tradesong.engine.RandomGenerator;
-import icbat.games.tradesong.engine.ScreenManager;
-import icbat.games.tradesong.engine.SimpleScreenManager;
+import com.badlogic.gdx.files.FileHandle;
+import icbat.games.tradesong.engine.*;
 import icbat.games.tradesong.engine.screens.OverviewScreen;
 import icbat.games.tradesong.game.Item;
 import icbat.games.tradesong.game.PlayerHoldings;
@@ -19,7 +17,9 @@ import icbat.games.tradesong.game.workshops.MutatorWorkshop;
 import icbat.games.tradesong.game.workshops.ProducerWorkshop;
 import icbat.games.tradesong.game.workshops.StorefrontWorkshop;
 import icbat.games.tradesong.game.workshops.Workshop;
+import org.apache.commons.io.FileUtils;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -59,9 +59,20 @@ public class TradesongGame extends Game {
 		basicItem = new Item("an Item", 300);
 		betterItem = new Item("a better item", 1000);
 		assembledItem = new Item("Assembled thing", 1500);
-		items.add(basicItem);
-		items.add(betterItem);
-		items.add(assembledItem);
+		items.addAll(new ItemJsonReader().read(readAssetFileToString("items.json")));
+		System.out.println(items);
+	}
+
+	private String readAssetFileToString(String path) {
+		final FileHandle assetFile = Gdx.files.internal(path);
+		final String string;
+		try {
+			string = FileUtils.readFileToString(assetFile.file());
+		} catch (IOException e) {
+			Gdx.app.error("Loading items", "IO error reading from" +assetFile.file().getAbsolutePath(), e);
+			throw new RuntimeException(e); //TODO make this less dirty
+		}
+		return string;
 	}
 
 	public void setupWorkshops() {
